9148881f054d4b9c3af2be1abed0ba6fccbf3ee6
safwen-code <safwendjebbi1234@gmail.com>
2025-12-04 19:48:12 +0100

    add circular progress

--- frontend/src/components/Content/skills/Skills.jsx
+++ frontend/src/components/Content/skills/Skills.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { Divider, Typography } from '@mui/material'
+
+import CircularPrg from './CircularPrg'
 
 const Skills = () => {
-  return <div>Skills</div>
+  return (
+    <>
+      {/* CircularProgressbar fixed */}
+      <CircularPrg />
+      <Divider sx={{ borderColor: '#D74B76' }} />
+
+      {/* pogrsseve bar fixed */}
+      <Typography>progresseve bar</Typography>
+    </>
+  )
 }
 
 export default Skills
