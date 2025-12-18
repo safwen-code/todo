346cfab30dd49823e0bf34c3bdba28cf27d9fe36
safwen-code <safwendjebbi1234@gmail.com>
2025-12-18 21:14:07 +0100

    add fake data

---
+++ frontend/src/utils/data.js
@@ -0,0 +1,302 @@
+import DesignServicesIcon from '@mui/icons-material/DesignServices'
+import CodeIcon from '@mui/icons-material/Code'
+import AssignmentIcon from '@mui/icons-material/Assignment'
+import AppShortcutIcon from '@mui/icons-material/AppShortcut'
+
+export const profile = {
+  about: {
+    avatar: '',
+    name: 'John Doe',
+    jobs: [
+      'FullStack Developer',
+      'Web Developer',
+      'Software Engineer',
+      'Mobile Developer',
+    ],
+    links: [
+      { github: 'https://github.com/safwen-code' },
+      { linkedin: 'https://www.linkedin.com/in/safwen-djebbi-5a996b204/' },
+      { facebook: 'https://www.facebook.com/profile.php?id=100078734872887' },
+    ],
+    personaldata: {
+      num: '+178548966',
+      email: 'jhondo@gmai.com',
+      country: 'Tunisie, Ben Arous',
+      birth: '20/08/1994',
+    },
+    file: {},
+  },
+  description: {
+    profdescription:
+      'Développeur Full Stack passionné par l’univer du web et doté d’une curiosité pour cette metier.j’exerce cette metier depuis 2017 avec 3 ans d’experience. Je me suis spécialisé dans le développement frontend ansi que le backend et je suis capable d’adopter avec tous les environnements du développement. Je suis spécialisé sur les frameworks Symfony, React Native, React , Python, Php et Nodejs.',
+    whatido: [
+      {
+        title: 'UI UX',
+        img: <DesignServicesIcon />,
+        description:
+          'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica',
+      },
+      {
+        title: 'App Developpement',
+        img: <CodeIcon />,
+        description:
+          'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica',
+      },
+      {
+        title: 'Conception et developpment',
+        img: <AssignmentIcon />,
+        description:
+          'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica',
+      },
+      {
+        title: 'App Mobile DEV',
+        img: <AppShortcutIcon />,
+        description:
+          'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica',
+      },
+    ],
+  },
+  project: {
+    projectdescription:
+      'Software Engineer with 3 years Computer Science degree and more than 2 year of experience.As a dedicated problem solver , I have many skill in Javascript ,node js , php and supporting laguages/libraries/frameworks that solve real-word problems through code.',
+    projects: [
+      {
+        image: '',
+        title: 'Ecomerce app',
+        description: '',
+        likes: [
+          { id: '1', name: 'safwen' },
+          { id: '1', name: 'safwen' },
+          { id: '1', name: 'safwen' },
+        ],
+        addtocart: [],
+      },
+      {
+        image: '',
+        title: 'Mobile App Interview',
+        description: '',
+        likes: [
+          { id: '1', name: 'safwen' },
+          { id: '1', name: 'safwen' },
+          { id: '1', name: 'safwen' },
+        ],
+        addtocart: [],
+      },
+      {
+        image: '',
+        title: 'Jungel Home',
+        description: '',
+        likes: [
+          { id: '1', name: 'safwen' },
+          { id: '1', name: 'safwen' },
+          { id: '1', name: 'safwen' },
+        ],
+        addtocart: [],
+      },
+      {
+        image: '',
+        title: 'shippement Process',
+        description: '',
+        likes: [
+          { id: '1', name: 'safwen' },
+          { id: '1', name: 'safwen' },
+          { id: '1', name: 'safwen' },
+        ],
+        addtocart: [],
+      },
+    ],
+  },
+
+  professionalpath: {
+    eductaion: [
+      {
+        company: 'Gomaycode',
+        date: '07-10-2019 / 10-01-2020',
+        titleformation: 'Formation Développeur Web-Full Stack (MERN)',
+        workFor: [
+          'Maîtrise des technologies MongoDB, Express.js, React, Node.js (MERN)',
+          ' Mise en œuvre de projets pratiques, tels que des applications e-commerce, des blogs interactifs',
+          "Création et gestion de bases de données NoSQL avec MongoDB, Conception d'API RESTful et intégration backend avec Node.js et Express.js",
+          "Développement frontend interactif avec React.js, incluant hooks, context API et gestion de l'état",
+          'Experience of version management with Git and deployment in cloud platforms like Heroku or Netlify',
+        ],
+      },
+      {
+        company: 'Téchnicien Supérieur En Informatique',
+        date: '03-10-2017',
+        titleformation: 'Développeur du Systéme Informatique',
+        workFor: [
+          "Formation complète en concepts fondamentaux de l'informatique, incluant la programmation, les algorithmes, les systèmes d'information et les réseaux",
+          'Développement des compétences en analyse et résolution de problèmes techniques',
+          'Réalisation de projets académiques, mettant en pratique des langages comme C++, Java, ou Javascript',
+        ],
+      },
+      {
+        company: 'Baccalaureate informatique',
+        date: 'October 2013, 2014',
+      },
+    ],
+    experience: [
+      {
+        company: 'Gomaycode',
+        date: '07-10-2019 / 10-01-2020',
+        titleformation: 'Formation Développeur Web-Full Stack (MERN)',
+        workFor: [
+          'Surveillance et suivi de la production en temps réel',
+          ' Automatisation de la génération de tickets pour les emballages et palettes',
+          'Analyse des rebuts, suivi des PPM (parties par million), analyse de Pareto et capacités de production',
+          'Garantie de la qualité fournisseur et client',
+        ],
+        technologie: {
+          frontend: [
+            'HTML',
+            'SASS',
+            'JavaScript',
+            'jQuery',
+            'DOM',
+            'React',
+            'AJAX',
+            'Axios',
+          ],
+          backend: [
+            'PHP',
+            'Dyncase',
+            'Node.js',
+            'PostgreSQL',
+            'TCPDF',
+            'programmation orientée objet',
+          ],
+          other: ['ubento', 'jira', 'scrum', 'bigdata'],
+        },
+      },
+      {
+        company: 'Gomaycode',
+        date: '07-10-2019 / 10-01-2020',
+        titleformation: 'Instructeur Web',
+        workFor: [
+          'Surveillance et suivi de la production en temps réel',
+          ' Automatisation de la génération de tickets pour les emballages et palettes',
+          'Analyse des rebuts, suivi des PPM (parties par million), analyse de Pareto et capacités de production',
+          'Garantie de la qualité fournisseur et client',
+        ],
+        technologie: {
+          frontend: [
+            'HTML',
+            'SASS',
+            'JavaScript',
+            'jQuery',
+            'DOM',
+            'React',
+            'AJAX',
+            'Axios',
+          ],
+          backend: [
+            'PHP',
+            'Dyncase',
+            'Node.js',
+            'PostgreSQL',
+            'TCPDF',
+            'programmation orientée objet',
+          ],
+          other: ['ubento', 'jira', 'scrum', 'bigdata'],
+        },
+      },
+      {
+        company: 'TIMELEC',
+        date: '10-02-2020 27-11-21',
+        titleformation: 'Opérateur Machine',
+        workFor: [
+          'Operateur Du Montage TimelecVérification du produit',
+          'Assemblage du produit en utilisant le système FT bios',
+          "Identification et gestion des QRQS en cas d'erreur",
+          'Enregistrement des données sur la plateforme TRS',
+          'Utilisation de la méthodologie LEAN pour accroître l efficacité de l équipe',
+        ],
+      },
+    ],
+    internship: [
+      {
+        company: 'Network Expertise Tunisia',
+        date: '09-05-14 01-06-14',
+        titleformation: 'Stage d Initiation',
+        workFor: [
+          'Operateur Du Montage TimelecVérification du produit',
+          'AssemblagParticiper à la conception et au développement d applications informatiques en utilisant des technologies adaptées',
+          "Contribution à l'identification et à la résolution de problèmes techniques pour améliorer la performance des systèmes",
+          'Rédaction de rapports techniques et documentation des processus pour assurer un suivi et une amélioration continue des projets',
+        ],
+      },
+      {
+        company: 'Intercom Technologie',
+        date: '01-08-17 / 31-08-17',
+        titleformation: 'Développeur Web',
+        workFor: [
+          "Développement d'un système de gestion des ressources humaines, incluant la gestion des équipes (Comsis)",
+          "Mise en place d'un système de notifications par email pour informer les utilisateurs des statuts ANFR",
+          'Optimisation des processus internes via des solutions web adaptées aux besoins opérationnels',
+        ],
+      },
+    ],
+    certification: [
+      {
+        company: 'Intercom Technologie',
+        date: '01-08-17 / 31-08-17',
+        titleformation: 'Développeur Web',
+        workFor: [
+          "Développement d'un système de gestion des ressources humaines, incluant la gestion des équipes (Comsis)",
+          "Mise en place d'un système de notifications par email pour informer les utilisateurs des statuts ANFR",
+          'Optimisation des processus internes via des solutions web adaptées aux besoins opérationnels',
+        ],
+      },
+    ],
+  },
+
+  skills: {
+    softskill: [
+      { name: 'html', count: 100 },
+      { name: 'css', count: 60 },
+      { name: 'js', count: 75 },
+      { name: 'Dom', count: 70 },
+      { name: 'react', count: 80 },
+      { name: 'reactN', count: 60 },
+      { name: 'vuejs', count: 60 },
+      { name: 'angular', count: 50 },
+    ],
+    backendskill: [
+      { name: 'Php', progress: '70' },
+      { name: 'Node js', progress: '50' },
+      { name: 'C Sharp', progress: '90' },
+    ],
+    utils: [
+      'postman',
+      'vsCode',
+      'devExpress',
+      'jira',
+      ' flux',
+      ' git',
+      ' mongoose',
+      ' postgresql',
+      ' express',
+      ' python ',
+      'angular',
+      ' jwt',
+      ' redux',
+      'mvc',
+      'mvvm',
+    ],
+    softwaresViews: [
+      {
+        label: 'Framework I Use',
+        imgPath: ['ang', 'react', 'vue'],
+      },
+      {
+        label: 'backend Envirement I Use',
+        imgPath: ['nodejs', 'php', 'express'],
+      },
+      {
+        label: 'Database I Use',
+        imgPath: ['mongodb', 'mysql', 'graph'],
+      },
+    ],
+  },
+}
